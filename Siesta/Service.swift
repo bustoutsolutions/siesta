cba1b594497b1e4665ea07ef4d2b4a9d14584a64
Paul Cantrell <cantrell@pobox.com>
2015-06-15 15:57:05 -0500

    Carthage, Quick, Nimble

---
+++ Siesta/Service.swift
@@ -0,0 +1,14 @@
+//
+//  Service.swift
+//  Siesta
+//
+//  Created by Paul on 2015/6/15.
+//  Copyright © 2015 Bust Out Solutions. All rights reserved.
+//
+
+import UIKit
+
+public class Service: NSObject
+    {
+    public func hello() -> Int { return 2 }
+    }
--- SiestaTests/SiestaTests.swift
+++ SiestaTests/SiestaTests.swift
@@ -6,30 +6,20 @@
 //  Copyright © 2015 Bust Out Solutions. All rights reserved.
 //
 
-import XCTest
+import Siesta
+import Quick
+import Nimble
 
-class SiestaTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+class SiestaTests: QuickSpec
+    {
+    override func spec()
+        {
+        describe("Unit testing")
+            {
+            it("works")
+                {
+                expect(Service().hello()).to(equal(2))
+                }
+            }
         }
     }
-    
-}
