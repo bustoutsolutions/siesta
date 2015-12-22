fb8efabc613c1cf07f9740fce511c4eeb04d9266
Cédric Foellmi <cedric@onekilopars.ec>
2015-12-22 14:24:55 +0100

    Adding OSX target

---
+++ Siesta OSX/Siesta OSX.h
@@ -0,0 +1,19 @@
+//
+//  Siesta OSX.h
+//  Siesta OSX
+//
+//  Created by Cédric Foellmi on 21/12/15.
+//  Copyright © 2015 Bust Out Solutions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Siesta OSX.
+FOUNDATION_EXPORT double Siesta_OSXVersionNumber;
+
+//! Project version string for Siesta OSX.
+FOUNDATION_EXPORT const unsigned char Siesta_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Siesta_OSX/PublicHeader.h>
+
+
