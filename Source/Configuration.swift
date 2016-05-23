afb2d8dbb1bd9cc07bdce61edf1da8f5e3357d2a
Paul Cantrell <cantrell@pobox.com>
2016-05-22 21:45:10 -0500

    Docs for method-based config gotchas & corner cases

--- Source/Configuration.swift
+++ Source/Configuration.swift
@@ -13,10 +13,15 @@
 */
 public struct Configuration
     {
+    // MARK: General Resource Behavior
+
     /**
       Time before valid data is considered stale by `Resource.loadIfNeeded()`.
 
       The default is 30 seconds.
+
+      - Note: This property is configured at the resource level, and does not depend on the HTTP method of any request.
+        Siesta uses the value configured for GET; if you override this for other HTTP methods, Siesta will ignore it.
     */
     public var expirationTime: NSTimeInterval = 30
 
@@ -24,11 +29,29 @@ public struct Configuration
       Time `Resource.loadIfNeeded()` will wait before allowing a retry after a failed request.
 
       The default is 1 second.
+
+      - Note: This property is configured at the resource level, and does not depend on the HTTP method of any request.
+        Siesta uses the value configured for GET; if you override this for other HTTP methods, Siesta will ignore it.
     */
     public var retryTime: NSTimeInterval = 1
 
     /**
+      An optional store to maintain the state of resources between app launches.
+
+      - Note: This property is configured at the resource level, and does not depend on the HTTP method of any request.
+        Siesta uses the value configured for GET; if you override this for other HTTP methods, Siesta will ignore it.
+    */
+    public var persistentCache: EntityCache? = nil
+
+    // MARK: Request Handling
+
+    /**
       Default request headers.
+
+      - Note: `Resource.request(...)` accepts a `requestMutation` closure than can change the HTTP method of a request.
+        If you override configuration based on HTTP method, then unlike other configuration properties, `headers`
+        depends on the initially requested, _pre-mutation_ method of a request. All other configuration properties
+        will depend on the _post-mutation_ request method.
     */
     public var headers: [String:String] = [:]
 
@@ -59,16 +82,13 @@ public struct Configuration
     public var responseTransformers: TransformerSequence = TransformerSequence()
 
     /**
-      An optional store to maintain the state of resources between app launches.
-    */
-    public var persistentCache: EntityCache? = nil
-
-    /**
       Interval at which request hooks & observers receive progress updates. This affects how frequently
       `Request.onProgress(_:)` and `ResourceObserver.resourceRequestProgress(_:progress:)` are called, and how often the
       `Request.progress` property (which is partially time-based) is updated.
     */
     public var progressReportingInterval: NSTimeInterval = 0.05
+
+    // MARK: Creating Configurations
 
     /**
       Holds a mutable configuration while closures passed to `Service.configure(...)` modify it.
