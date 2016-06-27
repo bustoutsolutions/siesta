c85a2d1285ba86c812d2cad6740b4802af538362
Paul Cantrell <cantrell@pobox.com>
2016-06-26 21:08:53 -0500

    Pipeline API & more comments in example app

--- Examples/GithubBrowser/Source/API/GithubAPI.swift
+++ Examples/GithubBrowser/Source/API/GithubAPI.swift
@@ -8,6 +8,8 @@ let GithubAPI = _GithubAPI()
 
 class _GithubAPI {
 
+    // MARK: Configuration
+
     private let service = Service(baseURL: "https://api.github.com")
 
     private init() {
@@ -18,19 +20,28 @@ class _GithubAPI {
         // Global configuration
 
         service.configure {
+            // basicAuthHeader property’s didSet causes this config to be reapplied whenever auth changes
+            
             $0.config.headers["Authorization"] = self.basicAuthHeader
-            $0.config.responseTransformers.add(GithubErrorMessageExtractor())
-            $0.config.responseTransformers.add(SwiftyJSONTransformer, contentTypes: ["*/json"])
+            
+            // By default, Siesta parses JSON using NSJSONSerialization. This example wraps that with SwiftyJSON.
+            
+            $0.config.pipeline[.parsing].add(SwiftyJSONTransformer, contentTypes: ["*/json"])
+            
+            // Custom transformers can change any response into any other — in this case, replacing the default error
+            // message with the one provided by the Github API.
+            
+            $0.config.pipeline[.cleanup].add(GithubErrorMessageExtractor())
         }
         
         // Mapping from specific paths to models
 
         service.configureTransformer("/users/*") {
-            User(json: $0.content)
+            User(json: $0.content)  // Input type inferred because User.init takes JSON
         }
         
         service.configureTransformer("/users/*/repos") {
-            ($0.content as JSON).arrayValue.map(Repository.init)
+            ($0.content as JSON).arrayValue.map(Repository.init)  // “as JSON” gives Siesta an explicit input type
         }
         
         // Note that you can use Siesta without these sorts of model mappings. By default, Siesta parses JSON, text,
@@ -41,6 +52,8 @@ class _GithubAPI {
         // the expected type becomes an error. For example, "/users/foo" must return a JSON response because that's
         // what the User(json:) expects.
     }
+    
+    // MARK: Authentication
     
     func logIn(username username: String, password: String) {
         if let auth = "\(username):\(password)".dataUsingEncoding(NSUTF8StringEncoding) {
@@ -58,12 +71,20 @@ class _GithubAPI {
 
     private var basicAuthHeader: String? {
         didSet {
+            // These two calls are almost always necessary when you have changing auth for your API:
+            
             service.invalidateConfiguration()  // So that future requests for existing resources pick up config change
             service.wipeResources()            // Scrub all unauthenticated data
+            
+            // Note that wipeResources() broadcasts a “no data” event to all observers of all resources.
+            // Therefore, if your UI diligently observes all the resources it uses, this call prevents sensitive data
+            // from lingering in the UI after logout.
         }
     }
 
-    // Resource convenience accessors
+    // MARK: Resource convenience accessors
+    
+    // A set of lightweight wrappers that return Siesta resources turn your REST API into a nice Swift API.
 
     func user(username: String) -> Resource {
         return service.resource("users").child(username.lowercaseString)
