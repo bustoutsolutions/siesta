5a269a2e9fb798f277951c1372116edfea24d427
Ahmet Karalar <akaralar@gmail.com>
2016-07-17 06:28:01 +0300

    Add generic parameters to ResourceState, add hot signal for request progress

--- Extensions/ReactiveCocoa/Siesta+ReactiveCocoa.swift
+++ Extensions/ReactiveCocoa/Siesta+ReactiveCocoa.swift
@@ -11,33 +11,48 @@ import Siesta
 import ReactiveCocoa
 import Result
 
-public struct ResourceState
+public struct ResourceState<T>
     {
-    public var data: Entity?
+    public var content: T?
     public var error: Error?
     public var isLoading, isRequesting: Bool
     }
 
 public extension Resource
     {
-    var state: ResourceState
+    public func snapshot<T>()
+        -> ResourceState<T>
         {
-        return ResourceState(
-            data: latestData,
-            error: latestError,
+        let content: T? = latestData?.typedContent()
+        let contentTypeError: Error? =
+            (latestData != nil && content == nil)
+                ? Siesta.Error(
+                    userMessage: "The server return an unexpected response type",
+                    cause: Error.Cause.WrongContentType())
+                : nil
+
+        return ResourceState<T>(
+            content: content,
+            error: latestError ?? contentTypeError,
             isLoading: isLoading,
             isRequesting: isRequesting)
+
         }
     }
 
-public struct ReactiveObserver
+extension Siesta.Error.Cause
     {
-    public let signal: Signal<ResourceState, NoError>
-    private let observer: Observer<ResourceState, NoError>
+    public struct WrongContentType: ErrorType { }
+    }
+
+public struct ReactiveObserver<T>
+    {
+    public let signal: Signal<ResourceState<T>, NoError>
+    private let observer: Observer<ResourceState<T>, NoError>
 
     public init()
         {
-        (signal, observer) = Signal<ResourceState, NoError>.pipe()
+        (signal, observer) = Signal<ResourceState<T>, NoError>.pipe()
         }
     }
 
@@ -45,17 +60,17 @@ extension ReactiveObserver: ResourceObserver
     {
     public func resourceChanged(resource: Resource, event: ResourceEvent)
         {
-        observer.sendNext(resource.state)
+        observer.sendNext(resource.snapshot())
         }
     }
 
 public extension Resource
     {
-    public func rac_signal(
+    public func rac_signal<T>(
             owner: AnyObject)
-        -> Signal<ResourceState, NoError>
+        -> Signal<ResourceState<T>, NoError>
         {
-        let reactiveObserver = ReactiveObserver()
+        let reactiveObserver = ReactiveObserver<T>()
         self.addObserver(reactiveObserver, owner: owner)
         return reactiveObserver.signal
         }
@@ -86,6 +101,23 @@ public extension Request
                 [weak self] in
                 self?.cancel()
                 }
+            }
+        }
+    
+    public func rac_progress()
+        -> Signal<Double, NoError>
+        {
+        return Signal
+            {
+            [weak self] observer in
+            self?
+                .onProgress
+                    {
+                    progress in
+                    observer.sendNext(progress)
+                    if progress == 1 { observer.sendCompleted() }
+                    }
+            return nil
             }
         }
     }
