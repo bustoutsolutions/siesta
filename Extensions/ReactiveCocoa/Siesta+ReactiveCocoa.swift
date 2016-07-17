8a7233a49edf1b6aba715d12069fb30cb93c1e47
Ahmet Karalar <akaralar@gmail.com>
2016-07-17 04:43:28 +0300

    Get the cold signal for the request from an extension on Request

--- Extensions/ReactiveCocoa/Siesta+ReactiveCocoa.swift
+++ Extensions/ReactiveCocoa/Siesta+ReactiveCocoa.swift
@@ -52,27 +52,25 @@ extension ReactiveObserver: ResourceObserver
 
 public extension Resource
     {
-
     public func rac_signal(
-            owner: AnyObject)
-        -> Signal<ResourceState, NoError>
+        owner: AnyObject)
+            -> Signal<ResourceState, NoError>
         {
         let reactiveObserver = ReactiveObserver()
         self.addObserver(reactiveObserver, owner: owner)
         return reactiveObserver.signal
         }
-    
-    public func rac_request(
-            method: RequestMethod,
-            @noescape requestMutation: NSMutableURLRequest -> () = { _ in })
+    }
+
+public extension Request
+    {
+    public func rac_signal()
         -> SignalProducer<Entity, Error>
         {
-        let nsreq = nsreqWith(method, requestMutation: requestMutation)
         return SignalProducer
             {
             [weak self] observer, disposable in
-            
-            let req = self!.requestWith(nsreq)
+            self?
                 .onSuccess
                     {
                     entity in
@@ -84,133 +82,12 @@ public extension Resource
                     error in
                     observer.sendFailed(error)
                     }
-
             disposable.addDisposable
                 {
-                req.cancel()
+                [weak self] in
+                self?.cancel()
                 }
             }
         }
     }
 
-// MARK - Request Creation Convenience
-
-public extension Resource {
-    public func rac_request(
-            method: RequestMethod,
-            data: NSData,
-            contentType: String,
-            @noescape requestMutation: NSMutableURLRequest -> () = { _ in })
-        -> SignalProducer<Entity, Error>
-        {
-        return rac_request(method)
-            {
-            nsreq in
-            nsreq.addValue(contentType, forHTTPHeaderField: "Content-Type")
-            nsreq.HTTPBody = data
-                
-            requestMutation(nsreq)
-            }
-        }
-
-    public func rac_request(
-            method: RequestMethod,
-            text: String,
-            contentType: String = "text/plain",
-            encoding: NSStringEncoding = NSUTF8StringEncoding,
-            @noescape requestMutation: NSMutableURLRequest -> () = { _ in })
-        -> SignalProducer<Entity, Error>
-        {
-        let encodingName = CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(encoding))
-        guard let rawBody = text.dataUsingEncoding(encoding) else
-            {
-            return SignalProducer(
-                error: Error(
-                    userMessage: NSLocalizedString("Cannot send request", comment: "userMessage"),
-                    cause: Error.Cause.UnencodableText(encodingName: encodingName as String,
-                                                       text: text)))
-            }
-
-        return rac_request(method,
-                           data: rawBody,
-                           contentType: "\(contentType); charset=\(encodingName)")
-        }
-
-    public func rac_request(
-            method: RequestMethod,
-            json: NSJSONConvertible,
-            contentType: String = "application/json",
-            @noescape requestMutation: NSMutableURLRequest -> () = { _ in })
-        -> SignalProducer<Entity, Error>
-        {
-        guard NSJSONSerialization.isValidJSONObject(json) else
-            {
-            return SignalProducer(
-                error: Error(
-                    userMessage: NSLocalizedString("Cannot send request", comment: "userMessage"),
-                    cause: Error.Cause.InvalidJSONObject()))
-            }
-
-        do  {
-            let rawBody = try NSJSONSerialization.dataWithJSONObject(json, options: [])
-            return rac_request(method, data: rawBody, contentType: contentType)
-            }
-        catch
-            {
-            // Swift doesn’t catch NSInvalidArgumentException, so the isValidJSONObject() method above is necessary
-            // to handle the case of non-encodable input. Given that, it's unclear what other circumstances would cause
-            // encoding to fail such that dataWithJSONObject() is declared “throws” (radar 21913397, Apple-rejected!),
-            // but we catch the exception anyway instead of using try! and crashing.
-
-            return SignalProducer(
-                error: Error(
-                    userMessage: NSLocalizedString("Cannot send request", comment: "userMessage"),
-                    cause: error))
-            }
-
-        }
-
-    public func rac_request(
-            method: RequestMethod,
-            urlEncoded params: [String:String],
-            requestMutation: NSMutableURLRequest -> () = { _ in })
-        -> SignalProducer<Entity, Error>
-        {
-        func urlEscape(string: String) throws -> String
-            {
-            guard let escaped = string.stringByAddingPercentEncodingWithAllowedCharacters(Resource.allowedCharsInURLEncoding) else
-                {
-                throw Error.Cause.NotURLEncodable(offendingString: string)
-                }
-
-            return escaped
-            }
-
-        do  {
-            let paramString = try
-            params.map { try urlEscape($0.0) + "=" + urlEscape($0.1) }
-                  .sort()
-                  .joinWithSeparator("&")
-            return rac_request(method,
-                               data: paramString.dataUsingEncoding(NSASCIIStringEncoding)!,  // Reason for !: ASCII guaranteed safe because of escaping
-                               contentType: "application/x-www-form-urlencoded")
-            }
-        catch
-            {
-            return SignalProducer(
-                error: Error(
-                    userMessage: NSLocalizedString("Cannot send request", comment: "userMessage"),
-                    cause: error))
-            }
-        }
-
-    private static let allowedCharsInURLEncoding: NSCharacterSet =
-        {
-            // Based on https://github.com/Alamofire/Alamofire/blob/338955a54722dea6051ed5c5c76a8736f4195515/Source/ParameterEncoding.swift#L186
-            let charsToEscape = ":#[]@!$&'()*+,;="
-            let allowedChars = NSCharacterSet.URLQueryAllowedCharacterSet().mutableCopy()
-                as! NSMutableCharacterSet  // Reason for !: No typesafe NSMutableCharacterSet copy constructor
-            allowedChars.removeCharactersInString(charsToEscape)
-            return allowedChars
-        }()
-}
