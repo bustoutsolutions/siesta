44cad5a710a93c7e5d5a4bd230528aa6e12633dc
Paul Cantrell <cantrell@pobox.com>
2016-08-15 00:42:22 -0500

    Ω prefix forces CocoaPods to build Deprecations.swift last

--- Source/Support/Ω_Deprecations.swift
+++ Source/Support/Ω_Deprecations.swift
@@ -1,5 +1,5 @@
 //
-//  Deprecations.swift
+//  ΩDeprecations.swift  ← Ω prefix forces CocoaPods to build this last, which matters for nested type extensions
 //  Siesta
 //
 //  Created by Paul on 2015/12/12.
