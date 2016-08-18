2b93d1503527a7ac330fdd1b44acdf11d8e4f1af
Paul Cantrell <cantrell@pobox.com>
2016-08-17 23:59:37 -0500

    Moved UI helpers to separate SiestaUI module

---
+++ Source/Siesta/Support/SiestaUI.h
@@ -0,0 +1,16 @@
+//
+//  SiestaUI.h
+//  Siesta
+//
+//  Created by Paul on 2015/6/14.
+//  Copyright © 2016 Bust Out Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SiestaUI.
+FOUNDATION_EXPORT double SiestaUIVersionNumber;
+
+//! Project version string for SiestaUI.
+FOUNDATION_EXPORT const unsigned char SiestaUIVersionString[];
+
