b81efc074c9756c2a4316ce345597ce6a2276a5c
Paul Cantrell <cantrell@pobox.com>
2016-09-28 11:34:58 -0500

    Reworked perf tests: shared setup, large & small observer scenarios

--- Tests/PerformanceTests.swift
+++ Tests/PerformanceTests.swift
@@ -12,6 +12,15 @@ import Siesta
 
 class SiestaPerformanceTests: XCTestCase
     {
+    var service: Service!
+    var networkStub: NetworkStub!
+
+    override func setUp()
+        {
+        networkStub = NetworkStub()
+        service = Service(baseURL: "http://test.ing", networking: networkStub)
+        }
+
     func testGetExistingResources()
         {
         measure { self.exerciseResourceCache(uniqueResources: 20, iters: 20000) }
@@ -29,52 +38,68 @@ class SiestaPerformanceTests: XCTestCase
 
     func exerciseResourceCache(uniqueResources: Int, iters: Int, countLimit: Int = 100000)
         {
-        let service = stubbedServce()
+        setUp()  // Important to start empty each time
         service.cachedResourceCountLimit = countLimit
         for n in 0 ..< iters
             { _ = service.resource("/items/\(n % uniqueResources)") }
         }
 
-    func testObserverChurn()
+    func testObserverChurn5()
         {
-        let resource = stubbedServce().resource("/observed")
-        let observerCount = 5
+        measure { self.churnItUp(observerCount: 5, reps: 2000) }
+        }
+
+    func testObserverChurn100()
+        {
+        measure { self.churnItUp(observerCount: 100, reps: 1000) }
+        }
+
+    private func churnItUp(observerCount: Int, reps: Int)
+        {
+        // Note no setUp() per measure() here. We expect stable performance here even
+        // with an existing resource. Lack of that will show as high stdev in test results.
+
+        let resource = service.resource("/observed")
         let observers = (1...observerCount).map { _ in TestObserver() }
-        measure
+
+        for n in 0 ..< reps
             {
-            for n in 0 ..< 2000
-                {
-                var x = 0
-                resource.addObserver(observers[n % observerCount])
-                resource.addObserver(owner: observers[(n * 7) % observerCount])
-                    { _ in x += 1 }
-                resource.removeObservers(ownedBy: observers[(n * 3) % observerCount])
-                }
+            var x = 0
+            resource.addObserver(observers[n % observerCount])
+            resource.addObserver(owner: observers[(n * 7) % observerCount])
+                { _ in x += 1 }
+            resource.removeObservers(ownedBy: observers[(n * 3) % observerCount])
             }
         }
 
-    func testObserverOwnerChurn()
+    func testObserverOwnerChurn5()
         {
-        let resource = stubbedServce().resource("/observed")
-        let observerCount = 5
+        measure { self.churnOwnersUp(observerCount: 5, reps: 2000) }
+        }
+
+    func testObserverOwnerChurn100()
+        {
+        measure { self.churnOwnersUp(observerCount: 100, reps: 1000) }
+        }
+
+    private func churnOwnersUp(observerCount: Int, reps: Int)
+        {
+        let resource = service.resource("/observed")
         var observers = (1...observerCount).map { _ in TestObserver() }
 
-        measure
+        for n in 0 ..< reps
             {
-            for n in 0 ..< 100
-                {
-                var x = 0
-                resource.addObserver(observers[n % observerCount])
-                resource.addObserver(owner: observers[(n * 7) % observerCount])
-                    { _ in x += 1 }
-                observers[(n * 3) % observerCount] = TestObserver()
-                }
+            var x = 0
+            resource.addObserver(observers[n % observerCount])
+            resource.addObserver(owner: observers[(n * 7) % observerCount])
+                { _ in x += 1 }
+            observers[(n * 3) % observerCount] = TestObserver()
             }
         }
 
     func testRequestHooks()
         {
-        let resource = stubbedServce().resource("/hooked")
+        let resource = service.resource("/hooked")
         measure
             {
             for _ in 0 ..< 100
@@ -93,11 +118,9 @@ class SiestaPerformanceTests: XCTestCase
     func testObserverNotifications()
         {
         let resourceCount = 50
-        var responseStubs: [String:ResponseStub] = [:]
         for n in stride(from: 0, to: resourceCount, by: 2)
-            { responseStubs["/zlerp\(n)"] = ResponseStub(data: Data()) }
+            { networkStub.responses["/zlerp\(n)"] = ResponseStub(data: Data()) }
 
-        let service = stubbedServce(responses: responseStubs)
         let resources = (0 ..< resourceCount).map
             {
             service
@@ -120,29 +143,20 @@ class SiestaPerformanceTests: XCTestCase
         }
     }
 
-func stubbedServce(responses: [String:ResponseStub] = [:]) -> Service
-    {
-    return Service(baseURL: "http://test.ing", networking: NetworkStub(responses: responses))
-    }
-
 struct NetworkStub: NetworkingProvider
     {
-    var responses: [String:ResponseStub]
+    var responses: [String:ResponseStub] = [:]
     let dummyHeaders =
         [
         "A-LITTLE": "madness in the Spring",
         "Is-wholesome": "even for the King",
         "But-God-be": "with the Clown",
-
         "Who-ponders": "this tremendous scene",
         "This-whole": "experiment of green",
         "As-if-it": "were his own!",
 
         "X-Author": "Emily Dickinson"
         ]
-
-    init(responses: [String:ResponseStub])
-        { self.responses = responses }
 
     func startRequest(
             _ request: URLRequest,
