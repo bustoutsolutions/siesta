9974bc9021d0bfc8b9b0d2c46b7345ba1d0cac36
Andrew Reed <andrew_reed@hotmail.com>
2016-10-31 19:41:48 +0000

    added suggested pr changes

--- Source/SiestaUI/NetworkActivityIndicator.swift
+++ Source/SiestaUI/NetworkActivityIndicator.swift
@@ -1,5 +1,5 @@
 //
-//  Siesta+NetworkActivityIndicator.swift
+//  NetworkActivityIndicator.swift
 //  Siesta
 //
 //  Created by Andrew Reed on 30/10/2016.
@@ -10,36 +10,44 @@ import Siesta
 import UIKit
 
 /// Set a variable to count the requests and then we know when to show the network activity indicator.
-private var requestsInProgress = 0 {
-    didSet {
+private var requestsInProgress = 0
+    {
+    didSet
+        {
         UIApplication.shared.isNetworkActivityIndicatorVisible =
-        requestsInProgress > 0
+            requestsInProgress > 0
+        }
     }
-}
 
-extension Service {
+extension Service
+    {
     
-    private func requestStarted() {
+    private func requestStarted()
+        {
         requestsInProgress += 1
-    }
+        }
     
-    private func requestCompleted() {
+    private func requestCompleted()
+        {
         requestsInProgress -= 1
-    }
+        }
 
     /**
      On each request we will show the network activity indicator.
      */
-    public func showRequestsWithNetworkActivityIndicator() {
-        configure {
-            $0.decorateRequests {
+    public func showRequestsWithNetworkActivityIndicator()
+        {
+        configure
+            {
+            $0.decorateRequests
+                {
                 res, req in
                 
                 self.requestStarted()
                 req.onCompletion { _ in self.requestCompleted() }
                 
                 return req
+                }
             }
         }
     }
-}
