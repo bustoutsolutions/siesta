2e5d9643b8b49b812de204c266f316dd0290ad0b
Paul Cantrell <cantrell@pobox.com>
2017-04-24 21:41:58 -0500

    SwiftPM manifest should ignore tests for now

--- Package.swift
+++ Package.swift
@@ -2,10 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "Siesta",
-
-    // testDependencies apparently doesn't work, and the projects we depend on
-    // don't seem to be SwiftPM-ready anyhow.
-
-    // One day, SwiftPM will support UIKit. Until then...
-    exclude: ["Source/SiestaUI"]
+    exclude: [
+        "Source/SiestaUI", // One day, SwiftPM will support UIKit. Until then...
+        "Tests"            // SwiftPM support for test-only dependencies like Quick is broken / nonexistent
+    ]
 )
