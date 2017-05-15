edb56ac8dfcb75acb4b756df85e91b896de1c75c
Josh Sklar <jrmsklar@gmail.com>
2017-05-14 22:54:36 -0400

    Revert "make httpResponse a let"
    This reverts commit 38e68efd9f611085ebbc60a7f4766b2f3afab0f9.

--- Source/Siesta/RequestError.swift
+++ Source/Siesta/RequestError.swift
@@ -39,7 +39,7 @@ public struct RequestError: Error
     public var userMessage: String
 
     /// The HTTP response if this error came from an HTTP response.
-    public let httpResponse: HTTPURLResponse?
+    public var httpResponse: HTTPURLResponse?
     
     /// The HTTP status code (e.g. 404) if this error came from an HTTP response.
     public var httpStatusCode: Int? {
@@ -94,7 +94,6 @@ public struct RequestError: Error
             cause: Error,
             entity: Entity<Any>? = nil)
         {
-        self.httpResponse = nil
         self.userMessage = userMessage
         self.cause = cause
         self.entity = entity
