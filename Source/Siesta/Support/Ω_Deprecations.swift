c3a6e633049ecaee943d7d9a566b089aaa35332f
Paul Cantrell <cantrell@pobox.com>
2017-08-04 20:44:26 -0500

    Standard transformers can now be specified individually

--- Source/Siesta/Support/Ω_Deprecations.swift
+++ Source/Siesta/Support/Ω_Deprecations.swift
@@ -8,3 +8,17 @@
 
 import Foundation
 
+extension Service
+    {
+    @available(*, deprecated: 0.99, message: "Use `standardTransformers:` instead of `useDefaultTransformers:`. Choices are `[.json, .text, .image]`; use [] for none")
+    public convenience init(
+            baseURL: URLConvertible? = nil,
+            useDefaultTransformers: Bool,
+            networking: NetworkingProviderConvertible = URLSessionConfiguration.ephemeral)
+        {
+        if useDefaultTransformers
+            { self.init(baseURL: baseURL, networking: networking) }
+        else
+            { self.init(baseURL: baseURL, standardTransformers: [], networking: networking) }
+        }
+    }
