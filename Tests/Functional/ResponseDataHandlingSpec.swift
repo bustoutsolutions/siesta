230545a6a91f91de6849558317fbced9f7ad1052
Paul Cantrell <cantrell@pobox.com>
2017-08-06 12:32:35 -0500

    Oops! Those were supposed to be assertions

--- Tests/Functional/ResponseDataHandlingSpec.swift
+++ Tests/Functional/ResponseDataHandlingSpec.swift
@@ -89,8 +89,8 @@ class ResponseDataHandlingSpec: ResourceSpecBase
                 expect(resource().latestError?.cause is RequestError.Cause.WrongInputTypeInTranformerPipeline) == true
                 if let wrongTypeError = resource().latestError?.cause as? RequestError.Cause.WrongInputTypeInTranformerPipeline
                     {
-                    print(wrongTypeError.expectedType == Data.self)
-                    print(wrongTypeError.actualType == String.self)
+                    expect(wrongTypeError.expectedType == Data.self).to(beTrue())
+                    expect(wrongTypeError.actualType == String.self).to(beTrue())
                     }
                 }
 
